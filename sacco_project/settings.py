e21f43650a7dfe9c0c88055d72d004c5a2df86cc
samuelcresco <batelugolobi@gmail.com>
2026-07-12 18:41:42 +0300

    Update settings.py

--- sacco_project/settings.py
+++ sacco_project/settings.py
@@ -86,7 +86,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CUSTOM SETTINGS
-LOGIN_URL = '/login/'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
